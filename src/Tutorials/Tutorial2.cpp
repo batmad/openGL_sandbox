22ebfa933f77d1f02af8c75236f6964ea0282288
batmad <twmw@mail.ru>
2018-04-21 17:41:52 +0900

    add transforms

--- src/Tutorials/Tutorial2.cpp
+++ src/Tutorials/Tutorial2.cpp
@@ -1,6 +1,9 @@
 #include "Tutorial2.h"
 #include "Tools.h"
 #include "stb_image.h"
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
 
 Tutorial2::Tutorial2()
 {
@@ -71,7 +74,20 @@ void Tutorial2::init()
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
+	glm::mat4 model;
+	model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	int modelLoc = glGetUniformLocation(shader->ID, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
+	glm::mat4 view;
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	int viewLoc = glGetUniformLocation(shader->ID, "view");
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(45.0f), 16.0f / 9.0f, 0.1f, 100.0f);
+	int projectionLoc = glGetUniformLocation(shader->ID, "projection");
+	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 }
 
 void Tutorial2::draw()
